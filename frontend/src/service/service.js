68a9ad4fa787c184cee5bf3ea8135a8b185baf7c
Imana1122 <imanalimbu@gmail.com>
2023-06-14 20:23:29 +0545

    error fixed

--- frontend/src/service/service.js
+++ frontend/src/service/service.js
@@ -1,2 +1,3 @@
-REACT_APP_SERVER_DOMAIN = "https://e-commerce-demo-jxu1.onrender.com";
-REACT_APP_ADMIN_EMAIL = "imanalimbu@gmail.com";
+export const REACT_APP_SERVER_DOMAIN =
+  "https://e-commerce-demo-jxu1.onrender.com";
+export const REACT_APP_ADMIN_EMAIL = "imanalimbu@gmail.com";
